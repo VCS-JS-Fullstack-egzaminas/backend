d5c74e9b75166af1ed4137b2bd1f37b62c71ebe4
Mantanavicius <mlantanavicius@gmail.com>
2024-08-14 11:44:04 +0300

    minor fix in server.js

--- server.js
+++ server.js
@@ -18,11 +18,7 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(cors());
-
-=======
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-
 
 //routes
 app.use("/api/listings", listings);
