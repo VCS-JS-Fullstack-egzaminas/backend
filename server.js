72d78ebd8dd9b93ac41deafe83990a1a96be3918
Petras Butkus <p.butkus360@gmail.com>
2024-08-16 13:55:41 +0300

    add temp cors options

--- server.js
+++ server.js
@@ -17,9 +17,16 @@ dotenv.config();
 //express app
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
